6b7cf89cb59cc1170527ea5949299513d783a74d
cassidoo <cassidoo@gmail.com>
2020-08-17 11:39:19 -0700

    Add certification to schema.js

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -5,6 +5,7 @@ import createSchema from 'part:@sanity/base/schema-creator';
 import schemaTypes from 'all:part:@sanity/base/schema-type';
 
 // We import object and document schemas
+import certification from './certification';
 import mission from './mission';
 import stage from './stage';
 import video from './video';
@@ -12,5 +13,5 @@ import video from './video';
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   name: 'default',
-  types: schemaTypes.concat([mission, stage, video]),
+  types: schemaTypes.concat([certification, mission, stage, video]),
 });
