6ac26be7a99198808a0c5dad96e1da9218fc1e12
cassidoo <cassidoo@gmail.com>
2020-09-18 11:32:19 -0700

    Make the mission description statically rendered

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,55 +1,61 @@
 import React from 'react';
 import { Layout } from '../components/layout';
 import Link from 'next/link';
-const ReactMarkdown = require('react-markdown/with-html')
+import renderToString from 'next-mdx-remote/render-to-string';
+import hydrate from 'next-mdx-remote/hydrate';
 
-export default function Home({ data }) {
+export default function Home({ missions }) {
   return (
     <Layout>
       <div style={{ textAlign: 'center' }}>
-      <h1>Jamstack Explorers: Mission Control</h1>
-      <section>
-        <p>
-          Greetings, fellow explorer! Welcome to your mission control — a place to learn about building for the web with modern tools and techniques. As a Jamstack Explorer, you can chart a course through new frameworks, through bright new tools and APIs, through <a href="https://netlify.com">Netlify</a>'s features and workflows, and through the very fabric of HTML, CSS, and JavaScript itself.
-        </p>
-        <p>
-          Choose your mission. Track your progress. Earn rewards. And join us as we explore the Jamstack together.
-        </p>
-      </section>
+        <h1>Jamstack Explorers: Mission Control</h1>
+        <section>
+          <p>
+            Greetings, fellow explorer! Welcome to your mission control — a
+            place to learn about building for the web with modern tools and
+            techniques. As a Jamstack Explorer, you can chart a course through
+            new frameworks, through bright new tools and APIs, through{' '}
+            <a href="https://netlify.com">Netlify</a>'s features and workflows,
+            and through the very fabric of HTML, CSS, and JavaScript itself.
+          </p>
+          <p>
+            Choose your mission. Track your progress. Earn rewards. And join us
+            as we explore the Jamstack together.
+          </p>
+        </section>
 
-      <section>
-        <h2>Missions</h2>
-        <p>
-          Here in Mission Control, you'll find missions covering all sorts of web development and Jamstack topics.
-        </p>
-        {data.data.allMission.map((mission, index) => (
-          <div key={index}>
-            <h3>
-              <Link href="/missions/[slug]" as={`/missions/${mission.slug.current}`}>
-                <a>{mission.title}</a>
-              </Link>
-            </h3>
-            <div className="mission-description">
-              <ReactMarkdown
-                source={mission.description}
-                escapeHtml={false}
-              />
+        <section>
+          <h2>Missions</h2>
+          <p>
+            Here in Mission Control, you'll find missions covering all sorts of
+            web development and Jamstack topics.
+          </p>
+          {missions.map((mission, index) => (
+            <div key={index}>
+              <h3>
+                <Link
+                  href="/missions/[slug]"
+                  as={`/missions/${mission.slug.current}`}
+                >
+                  <a>{mission.title}</a>
+                </Link>
+              </h3>
+              <div className="mission-description">
+                {hydrate(mission.renderedDescription, { components: {} })}
+              </div>
             </div>
-          </div>
-        ))}
-      </section>
+          ))}
+        </section>
 
-
-      <section>
-        <h2>Your progress</h2>
-        <p>
-          Track your progress through missions, stages, and certifications.
-        </p>
-        <p>
-          <a href="#">Log in</a> to track your progress and earn rewards.
-        </p>
-      </section>
-
+        <section>
+          <h2>Your progress</h2>
+          <p>
+            Track your progress through missions, stages, and certifications.
+          </p>
+          <p>
+            <a href="#">Log in</a> to track your progress and earn rewards.
+          </p>
+        </section>
       </div>
     </Layout>
   );
@@ -80,7 +86,18 @@ export const getStaticProps = async () => {
     }
   ).then((response) => response.json());
 
+  let promises = data.data.allMission.map(async (mission) => {
+    let str = mission.description;
+    let renderedDescription = await renderToString(str, {
+      components: {},
+    });
+
+    return { ...mission, renderedDescription };
+  });
+
+  let missions = await Promise.all(promises);
+
   return {
-    props: { data },
+    props: { missions },
   };
 };
