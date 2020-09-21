bee0078b01e3da6198e05c0b026ff61ec9960795
tzmanics <tzmanics@gmail.com>
2020-09-21 17:37:29 -0400

    feat: adds mission data context and function. dquote> dquote> Co-authored-by: Jason Lengstorf <jason.lengstorf@netlify.com> Co-authored-by: Cassidy Williams <cassidy@netlify.com>

---
+++ functions/get-mission-data.js
@@ -0,0 +1,30 @@
+const fetch = require("node-fetch");
+
+exports.handler = async () => {
+
+  const {data} = await fetch(
+    'https://q8efilev.api.sanity.io/v1/graphql/production/default',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          {
+            allMission {
+              title
+              description
+            }
+          }
+        `,
+        variables: {},
+      }),
+    }
+  ).then((response) => response.json());
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data.allMission)
+  }
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,9 @@
+import React from 'react';
+import {MissionsProvider} from '../context/missions';
+
 // global styles
 import '../styles/global.css';
 
 export default function Explorers({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <MissionsProvider><Component {...pageProps}/></MissionsProvider> 
 }
