8cf8acfa1442816b162e205871d1ffbc4f415b75
Cassidy Williams <cassidoo@users.noreply.github.com>
2020-09-23 12:10:28 -0700

    Fix #26: Statically render paths from missions (#85)
    * Set up 404 and skeleton lesson routes
    
    * Rename Layout and SEO pages back
    
    * Add path matching from Sanity in routes
    
    * Add context to getStaticProps

--- src/pages/learn/[...lesson].js
+++ src/pages/learn/[...lesson].js
@@ -1,14 +1,56 @@
-import { useRouter } from 'next/router';
-
-// /learn/{mission-name}/{lesson-name}
+// /learn/{mission-name}/{stage-name}
 // /learn/{mission-name}
-// /learn/{lesson-name}
+// /learn/{stage-name}
 
-const Lesson = () => {
-  const router = useRouter();
-  const { asPath } = router;
+const Lesson = ({ missionSlug }) => {
+  return <p>Lesson: {missionSlug}</p>;
+};
 
-  return <p>Lesson: {asPath}</p>;
+export const getStaticProps = async ({ ...ctx }) => {
+  // In here, we will do the logic to differentiate between
+  // missions and lessons, and send props to the Lesson
+  // component based on the route, and then render different
+  // page types based on that!
+
+  return {
+    props: {
+      missionSlug: ctx.params.lesson,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const data = await fetch(
+    'https://q8efilev.api.sanity.io/v1/graphql/production/default',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          {
+            allMission {
+              slug {
+                current
+              }
+            }
+          }
+        `,
+        variables: {},
+      }),
+    }
+  ).then((response) => response.json());
+
+  // Getting the mission paths from the slugs
+  let paths = data.data.allMission.map((p) => {
+    return `/learn/${p.slug.current}`;
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
 };
 
 export default Lesson;
