db75626482041b7f1ce1dba223afd57985d449ef
tzmanics <tzmanics@gmail.com>
2020-09-28 14:39:30 -0400

    adds more to marketingCopy schema

--- backend/schemas/marketingCopy.js
+++ backend/schemas/marketingCopy.js
@@ -4,9 +4,19 @@ export default {
   type: 'document',
   fields: [
     {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
       name: 'content',
       title: 'Content',
       type: 'markdown',
+    },
+    {
+      name: 'pagePath',
+      title: 'Page Path',
+      type: 'string',
     },
   ],
 };
