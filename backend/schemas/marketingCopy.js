76d949b4cc78a60670feba240d3830ae8a8ac226
tzmanics <tzmanics@gmail.com>
2020-09-28 09:56:15 -0400

    adds marketingCopy schema

---
+++ backend/schemas/marketingCopy.js
@@ -0,0 +1,12 @@
+export default {
+  name: 'marketingCopy',
+  title: 'Marketing Copy',
+  type: 'document',
+  fields: [
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'markdown',
+    },
+  ],
+};
