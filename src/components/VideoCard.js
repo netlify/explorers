e74b1c0ad3df423fa66cc82b8c2be0c190541012
Sarah Drasner <sarah.drasner@gmail.com>
2020-10-04 16:58:39 -0600

    work on styling for the video cards

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -2,21 +2,22 @@ import styles from './VideoCard.module.css';
 import Link from 'next/link';
 
 function VideoCard({ video }) {
-  console.log(video.title)
-
   const bkStyle = {
-    background: `url(${video.coverImage}) center center no-repeat`
+    background: `url(${ video.coverImage }) center top no-repeat`
   };
 
   return (
-    <div className={styles.card} style={ bkStyle }>
-      <div className={styles.cardinfo}>
+    <div className={ styles.card } style={ bkStyle }>
+      <div className={ styles.cardinfo }>
+        <div className={ styles.carddescription }>
+          <h3>{ video.title }</h3>
+          <Link href="/learn/[slug]" as={`/learn/${ video.slug }`}>
+            <a className="btn">
+              Watch Course
+            </a>
+          </Link>
+        </div>
         <div>{ video.body }</div>
-        <Link href="/learn/[slug]" as={`/learn/${video.slug}`}>
-          <a>
-            Watch Course
-          </a>
-        </Link>
       </div>
     </div>
   );
