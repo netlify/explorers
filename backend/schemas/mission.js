a7fe580b30701d76e0a272d3b834b28723a5ef36
Sarah Drasner <sarah.drasner@gmail.com>
2020-10-09 14:31:27 -0600

    Wire up the missions (#130)
    * add in some new parts of the schema for missions and validation
    
    * work on schemas and putting the right info into the video
    
    * some cleanup
    
    * make the image taller
    
    * forgot to update the missions page too
    
    * fix background image layout
    
    Co-authored-by: Jason Lengstorf <jason.lengstorf@netlify.com>

--- backend/schemas/mission.js
+++ backend/schemas/mission.js
@@ -7,11 +7,13 @@ export default {
       name: 'title',
       title: 'Mission Title',
       type: 'string',
+      validation: Rule => Rule.required().min(2).max(70)
     },
     {
       name: 'slug',
       title: 'Slug',
       type: 'slug',
+      validation: Rule => Rule.required(),
       options: {
         source: 'title',
         maxLength: 96,
@@ -24,6 +26,19 @@ export default {
       options: {
         minRows: 15,
       },
+    },
+    {
+      name: 'coverImage',
+      title: 'Cover Image',
+      type: 'image'
+    },
+    {
+      name: 'instructor',
+      title: 'Instructor',
+      type: 'reference',
+      to: [{
+        type: 'instructor'
+      }]
     },
     {
       name: 'stages',
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,53 +1,26 @@
 import Layout from '@components/Layout';
 import HomeHero from '@components/HomeHero';
-import Link from 'next/link';
 import renderToString from 'next-mdx-remote/render-to-string';
-import hydrate from 'next-mdx-remote/hydrate';
 import VideoCard from '@components/VideoCard';
 
 export default function Home({ missions }) {
-  const videoplaceholder = new Array(6).fill(1).map((e, i) => ({
-    id: i,
-    title: `Video Title ${i}`,
-    coverImage: `https://via.placeholder.com/450x200?text=Woo+Jamstack+Explorers`,
-    body: `Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.`,
-    instructor: `Tara Manicsic`,
-    slug: `learn-things`,
-    avatar: `https://via.placeholder.com/30`,
-  }));
-
   return (
     <Layout navtheme="light">
       <div>
         <HomeHero />
 
-        <section>
+        <section className="margintop-lg">
           <div className="sectioncontain">
             <h2>Missions</h2>
             <p>
               Here in Mission Control, you'll find missions covering all sorts
               of web development and Jamstack topics.
             </p>
-            {missions.map((mission, index) => (
-              <div key={index}>
-                <h3>
-                  <Link
-                    href="/learn/[slug]"
-                    as={`/learn/${mission.slug.current}`}
-                  >
-                    <a>{mission.title}</a>
-                  </Link>
-                </h3>
-                <div className="mission-description">
-                  {hydrate(mission.renderedDescription, { components: {} })}
-                </div>
-              </div>
-            ))}
           </div>
 
           <div className="row sectioncontain">
-            {videoplaceholder.map((video) => (
-              <VideoCard key={video.id} video={video} />
+            {missions.map((mission, index) => (
+              <VideoCard key={index} video={mission} />
             ))}
           </div>
         </section>
@@ -72,6 +45,19 @@ export const getStaticProps = async () => {
               description
               slug {
                 current
+              }
+              coverImage {
+                asset {
+                  url
+                }
+              }
+              instructor {
+                name
+                avatar {
+                  asset {
+                    url
+                  }
+                }
               }
             }
           }
