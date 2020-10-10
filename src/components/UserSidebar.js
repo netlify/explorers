720cd29ef379782cf0f3a6643298612793471199
Sarah Drasner <sarah.drasner@gmail.com>
2020-10-10 15:27:33 -0600

    user title and styling

--- src/components/UserSidebar.js
+++ src/components/UserSidebar.js
@@ -5,7 +5,9 @@ function UserSidebar({ user }) {
   return (
     <div className={styles.sidebar}>
       <div className={styles.section}>
-        <UserProfilesvg user={user} />
+        <UserProfilesvg user={user} className={styles.usersvg} />
+        <h3 className={styles.username}>{user.full_name}</h3>
+        <h4 className={styles.usertitle}>Captain</h4>
       </div>
     </div>
   );
