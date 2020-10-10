67b5c5990daf12927e555051d79c65dfcdce7b7a
tzmanics <tzmanics@gmail.com>
2020-10-10 17:09:14 -0400

    removes unneeded stages work

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { MissionsProvider } from '../context/missions';
-import { StagesProvider } from '../context/stages';
 
 // global styles
 import '../styles/global.css';
@@ -8,9 +7,7 @@ import '../styles/global.css';
 export default function Explorers({ Component, pageProps }) {
   return (
     <MissionsProvider>
-      <StagesProvider>
-        <Component {...pageProps} />
-      </StagesProvider>
+      <Component {...pageProps} />
     </MissionsProvider>
   );
 }
