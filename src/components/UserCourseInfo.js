2b155bcb8f7b9a90a275496818ab7a009c6d4694
Sarah Drasner <sarah.drasner@gmail.com>
2020-10-10 19:07:32 -0600

    work on courses listing a little more

--- src/components/UserCourseInfo.js
+++ src/components/UserCourseInfo.js
@@ -1,9 +1,19 @@
-// import styles from './UserCourseInfo.module.css';
+import styles from './UserCourseInfo.module.css';
 
 function UserCourseInfo({ course }) {
   return (
-    <div>
-      <p>{JSON.stringify(course, null, 2)}</p>
+    <div className={styles.coursecontainer}>
+      <div className={styles.coverimage}>
+        <img src={course.coverImage} width="50" height="50" />
+      </div>
+      <div className={styles.title}>
+        <p>{course.title}</p>
+      </div>
+      <div className={styles.instructor}>
+        <p>{course.instructor}</p>
+      </div>
+      <div className={styles.tracker}>
+      </div>
     </div>
   );
 }
