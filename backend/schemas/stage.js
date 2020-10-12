844dece1e7dd1ed641b35a0a964fb878234134dd
Jason Lengstorf <jason@lengstorf.com>
2020-10-12 15:28:05 -0700

    feat: add slugs to Stage schema

--- backend/schemas/stage.js
+++ backend/schemas/stage.js
@@ -7,6 +7,17 @@ export default {
       name: 'title',
       title: 'Title',
       type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      validation: (Rule) => Rule.required(),
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
     },
     {
       name: 'checkpoint',
