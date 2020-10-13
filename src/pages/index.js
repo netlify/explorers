9168e9e78b324d9ceaea086d756d619779872e46
Ben Hong <ben@bencodezen.io>
2020-10-13 17:21:01 -0400

    feature (#92): integrate marketing copy with home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
+import { sanityQuery } from '@util/sanity';
+import renderToString from 'next-mdx-remote/render-to-string';
+import hydrate from 'next-mdx-remote/hydrate';
+
 import Layout from '@components/Layout';
 import HomeHero from '@components/HomeHero';
 import MissionCard from '@components/MissionCard';
 import { useMissionsState } from '@context/missions';
 
-export default function Home() {
+export default function Home({ title, content }) {
   const { missions } = useMissionsState();
+  const pageContent = hydrate(content, {});
 
   return (
     <Layout navtheme="light">
@@ -13,11 +18,8 @@ export default function Home() {
 
         <section className="margintop-lg">
           <div className="sectioncontain">
-            <h2>Missions</h2>
-            <p>
-              Here in Mission Control, you'll find missions covering all sorts
-              of web development and Jamstack topics.
-            </p>
+            <h2>{title}</h2>
+            {pageContent}
           </div>
 
           <div className="row sectioncontain">
@@ -29,4 +31,32 @@ export default function Home() {
       </div>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const data = await sanityQuery({
+    query: `
+      {
+        allMarketingCopy(where: { pagePath: { eq: "/" } }) {
+          title
+          content
+          pagePath
+        }
+      }
+    `,
+    variables: {
+      path: '/',
+    },
+  });
+
+  const [pageData] = data.allMarketingCopy;
+
+  const renderedContent = await renderToString(pageData.content, {});
+
+  return {
+    props: {
+      title: pageData.title,
+      content: renderedContent,
+    },
+  };
 }
