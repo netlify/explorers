f6d9c06cc0392a9818aef2e568001b0b82336153
Ben Hong <ben@bencodezen.io>
2020-10-22 10:59:15 -0400

    feat (#122): add normalize css

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import { UserProvider } from '@context/user';
 import { StagesProvider } from '@context/stages';
 
 // global styles
+import '../styles/normalize.css';
 import '../styles/global.css';
 
 export default function Explorers({ Component, pageProps }) {
