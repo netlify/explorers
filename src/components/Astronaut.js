90b2efc811600f032e153b43b3a8f59ec540c67d
Ben Hong <ben@bencodezen.io>
2020-10-23 16:36:20 -0400

    feat (#113): use grid to align astronaut; but still WIP

--- src/components/Astronaut.js
+++ src/components/Astronaut.js
@@ -5,7 +5,7 @@ function Astronaut({ width }) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 576 478"
-      className={`${styles.svg} astronaut`}
+      className={`${styles['astronaut-svg']} astronaut`}
     >
       <defs>
         <linearGradient
