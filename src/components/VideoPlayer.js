2c07f02dc1e37bc829af610b0499e71ba9e3a870
Jason Lengstorf <jason@lengstorf.com>
2020-10-23 09:00:51 -0700

    fix: make sure activity hub is initialized before using

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -9,7 +9,7 @@ const VideoPlayer = ({ publicId }) => {
   useEffect(() => {
     const video = ref.current;
 
-    if (!video) return;
+    if (!video || !activity?.send) return;
 
     // 2 debounced functions to make sure both fire at least once
     const sendProgressDebounced = debounce(activity.send, 500);
@@ -20,6 +20,8 @@ const VideoPlayer = ({ publicId }) => {
         (event.target.currentTime / event.target.duration) * 100
       );
 
+      console.log('progress');
+
       sendProgressDebounced('video-progress', {
         videoId: publicId,
         percentage,
@@ -29,16 +31,18 @@ const VideoPlayer = ({ publicId }) => {
     const handleCompleted = (event) => {
       // if they’ve watched more than 95% of the video, call it done
       const isComplete =
-        (event.target.currentTime / event.target.duration) > 0.95;
+        event.target.currentTime / event.target.duration > 0.95;
 
       if (!isComplete) {
         return;
       }
 
+      console.log('completed');
       sendCompleteDebounced('video-complete', { videoId: publicId });
     };
 
     const handleWindowClose = () => {
+      console.log('window-close');
       const percentage = Math.round((video.currentTime / video.duration) * 100);
       activity.send('video-progress', {
         videoId: publicId,
@@ -61,11 +65,11 @@ const VideoPlayer = ({ publicId }) => {
       video.removeEventListener('pause', handleCompleted);
       video.removeEventListener('ended', handleCompleted);
     };
-  }, []);
+  }, [activity]);
 
   // TODO let's add support for smaller formats as well
   return (
-    <video controls id="lesson-video">
+    <video controls id="lesson-video" ref={ref}>
       <source
         src={`https://res.cloudinary.com/netlify/video/upload/ac_none/${publicId}.mp4`}
         type="video/mp4"
