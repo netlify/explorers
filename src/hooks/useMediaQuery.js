f8c2f6a8a43e5eeca498fbb4da1a4b544ae162b7
cassidoo <cassidoo@gmail.com>
2020-11-06 15:36:15 -0600

    Add media hook, hooks folder

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,21 @@
+// Hook for using media queries for conditional rendering
+// Usage example:
+// let isPageWide = useMedia('(min-width: 800px)')
+
+function useMedia(query) {
+  const [matches, setMatches] = useState(window.matchMedia(query).matches);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => {
+      setMatches(media.matches);
+    };
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [matches, query]);
+
+  return matches;
+}
