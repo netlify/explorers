885f4d35cc2cba567b20f49d2589b547fbb6f35a
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2020-11-06 16:46:43 -0600

    feat: Responsive navigation (#213)
    * Move Netlify logo to its own component
    
    * Add media hook, hooks folder
    
    * Make mobile menu generally work
    
    * Update nav styles, add themes
    
    Co-authored-by: Sarah Drasner <sarah.drasner@gmail.com>
    Co-authored-by: cassidoo <cassidoo@users.noreply.github.com>

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+// Hook for using media queries for conditional rendering
+// Usage example:
+// let isPageWide = useMediaQuery('(min-width: 800px)')
+
+export function useMediaQuery(query) {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => {
+      setMatches(media.matches);
+    };
+    media.addListener(listener);
+    return () => media.removeListener(listener);
+  }, [matches, query]);
+
+  return matches;
+}
