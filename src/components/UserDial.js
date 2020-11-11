8b1f3bc49be6618fe1ce14d4f4948a4bcc683060
Jason Lengstorf <jason@lengstorf.com>
2020-11-10 18:23:13 -0800

    feat: show cert download when progress hits 100%

--- src/components/UserDial.js
+++ src/components/UserDial.js
@@ -7,6 +7,18 @@ function UserDial() {
   const { user } = useUserState();
   const progress = user.activity.certificateProgress;
 
+  function getCertificate(event) {
+    event.preventDefault();
+
+    const certURL = new URL(
+      'https://azcertificate.azurewebsites.net/api/Certificate'
+    );
+    certURL.searchParams.set('name', user.full_name);
+    certURL.searchParams.set('date', new Date().toLocaleDateString());
+
+    window.location = certURL;
+  }
+
   useEffect(() => {
     gsap.set('.dial', {
       rotation: 180 * progress,
@@ -19,53 +31,77 @@ function UserDial() {
     });
   });
 
+  const isComplete = parseInt(progress) === 1;
+
   return (
-    <svg
-      viewBox="100 150 800 350"
-      xmlns="http://www.w3.org/2000/svg"
-      aria-labelledby="knob"
-      role="presentation"
-    >
-      <title id="knob">User Profile Dial</title>
-      <defs>
-        <clipPath id="clip">
-          <rect className="dial" x="0" y="0" width="1000" height="500" />
-        </clipPath>
-      </defs>
-
-      <g>
-        <circle className={styles.stroke1b} r="300" cx="500" cy="500" />
-        <circle className={styles.stroke1b} r="265" cx="500" cy="500" />
-        <circle className={styles.stroke2b} r="220" cx="500" cy="500" />
-        <circle className={styles.stroke3b} r="170" cx="500" cy="500" />
-        <circle className={styles.stroke1b} r="120" cx="500" cy="500" />
-      </g>
-
-      <g clipPath="url(#clip)">
-        <circle className={styles.stroke1} r="300" cx="500" cy="500" />
-        <circle className={styles.stroke1} r="265" cx="500" cy="500" />
-        <circle className={styles.stroke2} r="220" cx="500" cy="500" />
-        <circle className={styles.stroke3} r="170" cx="500" cy="500" />
-        <circle className={styles.stroke1} r="120" cx="500" cy="500" />
-      </g>
-
-      <g className="dial">
-        <line className={styles.line1} x1="200" x2="500" y1="500" y2="500" />
-        <circle className={styles.knob} r="6" cx="200" cy="500" />
-        <text
-          className="texttransform"
-          fontSize="20px"
-          fill="#ccc"
-          x="160"
-          y="510"
+    <div className={isComplete ? styles.complete : ''}>
+      {isComplete ? (
+        <>
+          <h4 className={styles.completeHeading}>You did it!</h4>
+          <img
+            className={styles.completeImage}
+            src="https://res.cloudinary.com/netlify/image/upload/q_auto,f_auto,w_200/v1605058349/explorers/boppin-pug.gif"
+            alt="a pug dancing happily"
+          />
+          <a href="#certificate" onClick={getCertificate}>
+            View and Download Your Certificate
+          </a>
+        </>
+      ) : (
+        <svg
+          viewBox="100 150 800 350"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-labelledby="knob"
+          role="presentation"
         >
-          {progress * 100}%
-        </text>
-      </g>
+          <title id="knob">User Profile Dial</title>
+          <defs>
+            <clipPath id="clip">
+              <rect className="dial" x="0" y="0" width="1000" height="500" />
+            </clipPath>
+          </defs>
 
-      <line className={styles.line1} x1="150" x2="850" y1="499" y2="499" />
-      <circle className={styles.knob} r="20" cx="500" cy="500" />
-    </svg>
+          <g>
+            <circle className={styles.stroke1b} r="300" cx="500" cy="500" />
+            <circle className={styles.stroke1b} r="265" cx="500" cy="500" />
+            <circle className={styles.stroke2b} r="220" cx="500" cy="500" />
+            <circle className={styles.stroke3b} r="170" cx="500" cy="500" />
+            <circle className={styles.stroke1b} r="120" cx="500" cy="500" />
+          </g>
+
+          <g clipPath="url(#clip)">
+            <circle className={styles.stroke1} r="300" cx="500" cy="500" />
+            <circle className={styles.stroke1} r="265" cx="500" cy="500" />
+            <circle className={styles.stroke2} r="220" cx="500" cy="500" />
+            <circle className={styles.stroke3} r="170" cx="500" cy="500" />
+            <circle className={styles.stroke1} r="120" cx="500" cy="500" />
+          </g>
+
+          <g className="dial">
+            <line
+              className={styles.line1}
+              x1="200"
+              x2="500"
+              y1="500"
+              y2="500"
+            />
+            <circle className={styles.knob} r="6" cx="200" cy="500" />
+            <text
+              className="texttransform"
+              fontSize="20px"
+              fill="#ccc"
+              x="160"
+              y="510"
+            >
+              {progress * 100}%
+            </text>
+          </g>
+
+          <line className={styles.line1} x1="150" x2="850" y1="499" y2="499" />
+          <circle className={styles.knob} r="20" cx="500" cy="500" />
+        </svg>
+      )}
+    </div>
   );
 }
 
