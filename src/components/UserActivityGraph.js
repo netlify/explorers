948d2bb408afd603b4556fcfb8e94a1f61f632c8
Ben Hong <ben@bencodezen.io>
2020-11-11 22:17:57 -0500

    feat (#116): make user profile page responsive (#243)
    Co-authored-by: Sarah Drasner <sarah.drasner@gmail.com>

--- src/components/UserActivityGraph.js
+++ src/components/UserActivityGraph.js
@@ -2,13 +2,30 @@ import styles from './UserActivityGraph.module.css';
 import { AreaChart } from 'react-chartkick';
 import { useUserState } from 'src/context/user';
 import 'chart.js';
+import { useMediaQuery } from '../hooks/useMediaQuery';
 
 function UserActivityGraph() {
   const { user } = useUserState();
 
+  const isLargeMobile = useMediaQuery('(min-width: 500px');
+  const isTablet = useMediaQuery('(min-width: 768px)');
+  const isDesktop = useMediaQuery('(min-width: 1000px');
+
+  const chartSize = () => {
+    if (isDesktop) {
+      return '600px';
+    } else if (isTablet) {
+      return '550px';
+    } else if (isLargeMobile) {
+      return '450px';
+    } else {
+      return '250px';
+    }
+  };
+
   const opts = {
     title: 'Videos Watched',
-    width: '600px',
+    width: chartSize(),
     height: '200px',
     points: false,
   };
