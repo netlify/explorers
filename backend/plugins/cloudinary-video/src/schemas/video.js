0566b8150061ed89628a24836bef4c266cd11eba
Jason Lengstorf <jason@lengstorf.com>
2020-11-12 12:56:52 -0800

    feat: set Cloudinary video details as text fields

--- backend/plugins/cloudinary-video/src/schemas/video.js
+++ backend/plugins/cloudinary-video/src/schemas/video.js
@@ -1,26 +1,27 @@
 import { Input } from '../components/input';
-import { Preview } from '../components/preview';
+// import { Preview } from '../components/preview';
 
 export default {
   name: 'cloudinary.video',
   type: 'object',
   title: 'Cloudinary Video',
+  description:
+    'Get the public ID, and full secure URL from Cloudinary after you upload at https://url.netlify.com/S1dhTGotD',
   fields: [
     {
       type: 'string',
-      name: 'asset_id',
-    },
-    {
-      type: 'string',
       name: 'public_id',
+      description:
+        'This will always start with "explorers/" — it’s the last part of the URL without the .mp4',
     },
     {
       type: 'string',
       name: 'url',
+      description: 'Click the link icon on your video to copy this.',
     },
   ],
-  inputComponent: Input,
-  preview: {
-    component: Preview,
-  },
+  // inputComponent: Input,
+  // preview: {
+  //   component: Preview,
+  // },
 };
