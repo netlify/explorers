c38ae5d41df14867163e8080358d8e03e43b7e8f
Ben Hong <ben@bencodezen.io>
2020-11-12 20:36:20 -0500

    refactor (#254): remove built-in certificate confetti

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,12 @@
 import { useEffect } from 'react';
-import {
-  launchFireworkConfetti,
-  launchSchoolPrideConfetti,
-} from '../util/confetti';
+import { launchSchoolPrideConfetti } from '../util/confetti';
 import styles from './Modal.module.css';
 import IconClose from './IconClose';
 import Link from 'next/link';
 
 function Modal({ mission, closeModal }) {
   useEffect(() => {
-    if (mission) {
-      launchSchoolPrideConfetti();
-    } else {
-      launchFireworkConfetti();
-    }
+    launchSchoolPrideConfetti();
 
     window.addEventListener('keyup', (event) => {
       if (event.key === 'Escape') {
@@ -28,24 +21,18 @@ function Modal({ mission, closeModal }) {
         <button className={styles['modal-close']} onClick={closeModal}>
           <IconClose />
         </button>
-        <p className={styles['icon-award']}>{mission ? '🏅' : '🏆'}</p>
+        <p className={styles['icon-award']}>🏅</p>
         <p className={styles['modal-heading']}>Congratulations!</p>
-        {mission ? (
-          <>
-            <p>
-              You just completed <br />
-              {mission.title}!
-            </p>
-            <Link href="/missions" as="/missions">
-              <a className="btn btnprimary">Embark on a new mission!</a>
-            </Link>
-          </>
-        ) : (
-          <p>Awesome work!</p>
-        )}
+        <p>
+          You just completed <br />
+          {mission.title}!
+        </p>
+        <Link href="/missions" as="/missions">
+          <a className="btn btnprimary">Embark on a new mission!</a>
+        </Link>
         <button
           className={styles['replay-button']}
-          onClick={mission ? launchSchoolPrideConfetti : launchFireworkConfetti}
+          onClick={launchSchoolPrideConfetti}
         >
           Replay confetti animation
         </button>
