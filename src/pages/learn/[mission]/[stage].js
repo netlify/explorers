105e4604495a5f1458417c59e9299b3bb8ec4cfa
Ben Hong <ben@bencodezen.io>
2020-11-14 08:57:36 -0500

    feat (#281): add seo for stage pages

--- src/pages/learn/[mission]/[stage].js
+++ src/pages/learn/[mission]/[stage].js
@@ -9,6 +9,7 @@ import { loadStageBySlug } from '@context/stages';
 import styles from './Stage.module.css';
 import { useState, useEffect } from 'react';
 import { useUserState } from '@context/user';
+import removeMarkdown from 'remove-markdown';
 
 export default function Stage({ mission, stage }) {
   const publicId = stage.content?.[0].cloudinaryVideo?.public_id;
@@ -16,6 +17,12 @@ export default function Stage({ mission, stage }) {
   const description = stage.content?.[0].body;
   const [missionComplete, setMissionComplete] = useState(false);
   const { user, getUser } = useUserState();
+
+  const pageMeta = {
+    title: `Jamstack Explorers - ${mission.title} - ${stage.title}`,
+    description: removeMarkdown(description),
+    image: mission.coverImage.asset.url,
+  };
 
   const closeModal = () => {
     setMissionComplete(false);
@@ -34,7 +41,7 @@ export default function Stage({ mission, stage }) {
   };
 
   return (
-    <Layout navtheme="dark">
+    <Layout navtheme="dark" pageMeta={pageMeta}>
       <section>
         <div
           className={`${styles['stage-content']} section-contain margintop-lg`}
