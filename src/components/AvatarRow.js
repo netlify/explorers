ef912b7bd80ab02f134942876b0d85ca13c22269
Amberley Romo <amberleyjohanna@gmail.com>
2020-11-17 14:37:17 -0600

    add alt text to avatar images in AvatarRow

--- src/components/AvatarRow.js
+++ src/components/AvatarRow.js
@@ -3,7 +3,7 @@ import styles from './AvatarRow.module.css';
 const AvatarRow = (props) => {
   return (
     <div className={styles['avatar-row']}>
-      <img src={props.src} className="avatar" />
+      <img alt={props.label} src={props.src} className="avatar" />
       <p className={styles['avatar-label']}>{props.label}</p>
     </div>
   );
