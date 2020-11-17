5eae5f454b8ccf6148244f823df60bf5dc8ef963
Ben Hong <ben@bencodezen.io>
2020-11-16 20:19:09 -0500

    fix (#338): update sitemap domain (#342)
    Co-authored-by: Jason Lengstorf <jason.lengstorf@netlify.com>

--- src/pages/learn/[mission]/[stage].js
+++ src/pages/learn/[mission]/[stage].js
@@ -41,7 +41,6 @@ export default function Stage({ mission, stage }) {
   const pageMeta = {
     title: `${stage.title} - ${mission.title} - Jamstack Explorers`,
     description: descriptionMeta,
-    image: mission.coverImage.asset.url,
     url: `${SITE_DOMAIN}/learn/${mission.slug.current}/${stage.slug.current}`,
     creator: instructorTwitterHandle
       ? `@${instructorTwitterHandle}`
