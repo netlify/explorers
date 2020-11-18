62147a10b72fe0f3155960c00e62fce14b10f6ef
Jason Lengstorf <jason.lengstorf@netlify.com>
2020-11-18 08:32:50 -0800

    fix: use notify channel (#407)
    Co-authored-by: Ben Hong <ben@bencodezen.io>

--- functions/cloudinary-eager-notification.js
+++ functions/cloudinary-eager-notification.js
@@ -4,17 +4,16 @@ exports.handler = async (event) => {
   const { eager = [] } = JSON.parse(event.body);
   const count = eager.length || 0;
 
-  // current status: https://media0.giphy.com/media/a2G83cq7gotnq/giphy-downsized-large.gif
-  // await fetch(
-  //   'https://hooks.slack.com/services/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',
-  //   {
-  //     method: 'POST',
-  //     headers: { 'Content-Type': 'application/json' },
-  //     body: JSON.stringify({
-  //       text: `Cloudinary finished encoding ${count} video assets`,
-  //     }),
-  //   }
-  // );
+  await fetch(
+    'https://hooks.slack.com/services/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',
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        text: `Cloudinary finished encoding ${count} video assets`,
+      }),
+    }
+  );
 
   return {
     statusCode: 200,
