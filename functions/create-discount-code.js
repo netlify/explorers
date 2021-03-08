a49d0150075a9f4bd27c9b662f26f7e77bbd85ee
Ben Hong <ben@bencodezen.io>
2021-03-08 15:40:10 -0500

    feature: add discount code with removal of old code

---
+++ functions/create-discount-code.js
@@ -0,0 +1,69 @@
+const { postToShopify } = require('./util/postToSpotify');
+
+exports.handler = async (event) => {
+  const payload = JSON.parse(event.body);
+
+  const newPriceRuleAndDiscountCode = await postToShopify({
+    query: `
+      mutation priceRuleCreate($priceRule: PriceRuleInput!) {
+        priceRuleCreate(priceRule: $priceRule) {
+          priceRule {
+            id
+          }
+          priceRuleDiscountCode {
+            id
+            code
+            usageCount
+          }
+          priceRuleUserErrors {
+            code
+            field
+            message
+          }
+        }
+      }
+    `,
+    // TODO: Make values dynamic
+    variables: {
+      priceRule: {
+        allocationLimit: 1,
+        allocationMethod: 'EACH',
+        customerSelection: {
+          forAllCustomers: true,
+        },
+        itemEntitlements: {
+          productIds: ['gid://shopify/Product/4871257882763'],
+        },
+        target: 'LINE_ITEM',
+        title: 'Free Stickers',
+        usageLimit: 1,
+        validityPeriod: {
+          start: '2021-03-08',
+        },
+        value: {
+          percentageValue: -100,
+        },
+      },
+      priceRuleDiscountCode: {
+        code: 'test456',
+      },
+    },
+  });
+
+  if (!newPriceRuleAndDiscountCode) {
+    return {
+      statusCode: 500,
+      body: 'Oh no! Creating a price rule did not work :(',
+    };
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept',
+    },
+    body: 'Totally Ok',
+  };
+};
