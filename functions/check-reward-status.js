1c2b07dd262fc6f305fd373bcdc40bc24ebd368a
Ben Hong <ben@bencodezen.io>
2021-03-15 16:50:58 -0400

    feature (#474): add check reward status logic

--- functions/check-reward-status.js
+++ functions/check-reward-status.js
@@ -1,12 +1,50 @@
-/*
+const { postToHasura } = require('./util/postToHasura');
 
-*/
+exports.handler = async (event) => {
+  const payload = JSON.parse(event.body);
+  const { discount_codes: discountCodes } = payload;
 
-exports.handler = async (...args) => {
-  console.log(args);
+  const allRewards = await postToHasura({
+    query: `
+      query RewardsList {
+        rewards {
+          reward_data
+          id
+        }
+      }
+    `,
+  });
+
+  discountCodes.forEach(async (discountCode) => {
+    const discountCodeMatch = allRewards.rewards.find(
+      (item) => item.reward_data.code === discountCode
+    );
+
+    if (discountCodeMatch) {
+      await postToHasura({
+        query: `
+          mutation MyMutation($reward_id: Int!) {
+            update_rewards(
+              where: {id: {_eq: $reward_id}},
+              _set: {is_claimed: true}
+            ) {
+              returning {
+                id
+                is_claimed
+                reward_data
+              }
+            }
+          }
+        `,
+        variables: {
+          reward_id: discountCodeMatch.id,
+        },
+      });
+    }
+  });
 
   return {
     statusCode: 200,
-    body: 'ok',
+    body: 'Checked reward',
   };
 };
