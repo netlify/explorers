ea0ee00ab00c2004240422ff2ce88da444558e9b
Ben Hong <ben@bencodezen.io>
2021-03-15 17:10:28 -0400

    feature (#474): add error handling

--- functions/check-reward-status.js
+++ functions/check-reward-status.js
@@ -15,31 +15,49 @@ exports.handler = async (event) => {
     `,
   });
 
-  discountCodes.forEach(async (discountCode) => {
-    const discountCodeMatch = allRewards.rewards.find(
-      (item) => item.reward_data.code === discountCode
-    );
+  if (!discountCodes) {
+    return {
+      statusCode: 500,
+      body: 'Failed to retrieve discount codes from Shopify',
+    };
+  }
 
-    if (discountCodeMatch) {
-      await postToHasura({
-        query: `
-          mutation MyMutation($reward_id: Int!) {
-            update_rewards(
-              where: {id: {_eq: $reward_id}},
-              _set: {is_claimed: true}
-            ) {
-              returning {
-                id
-                is_claimed
-                reward_data
+  if (!allRewards) {
+    return {
+      statusCode: 500,
+      body: 'Failed to retrieve rewards from Hasura',
+    };
+  }
+
+  discountCodes.forEach(async (discountCode) => {
+    try {
+      const discountCodeMatch = allRewards.rewards.find(
+        (item) => item.reward_data.code === discountCode
+      );
+
+      if (discountCodeMatch) {
+        await postToHasura({
+          query: `
+            mutation MyMutation($reward_id: Int!) {
+              update_rewards(
+                where: {id: {_eq: $reward_id}},
+                _set: {is_claimed: true}
+              ) {
+                returning {
+                  id
+                  is_claimed
+                  reward_data
+                }
               }
             }
-          }
-        `,
-        variables: {
-          reward_id: discountCodeMatch.id,
-        },
-      });
+          `,
+          variables: {
+            reward_id: discountCodeMatch.id,
+          },
+        });
+      }
+    } catch (err) {
+      console.error('Failure to update rewards: ', err);
     }
   });
 
