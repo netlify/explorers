a6195895ce24e0b6d8ad2b1583d3029a4c71835a
Ben Hong <ben@bencodezen.io>
2021-03-15 14:43:08 -0400

    refactor: add try catch to improve code thanks to Sarah

--- functions/util/createShopifyDiscountCode.js
+++ functions/util/createShopifyDiscountCode.js
@@ -2,56 +2,60 @@ const { postToShopify } = require('./postToShopify');
 const { v4: uuidv4 } = require('uuid');
 
 exports.createShopifyDiscountCode = async (achievementId) => {
-  const newDiscountCode = await postToShopify({
-    query: `
-      mutation priceRuleCreate(
-        $priceRule: PriceRuleInput!,
-        $priceRuleDiscountCode: PriceRuleDiscountCodeInput!
-      ) {
-        priceRuleCreate(
-          priceRule: $priceRule,
-          priceRuleDiscountCode: $priceRuleDiscountCode
+  try {
+    const newDiscountCode = await postToShopify({
+      query: `
+        mutation priceRuleCreate(
+          $priceRule: PriceRuleInput!,
+          $priceRuleDiscountCode: PriceRuleDiscountCodeInput!
         ) {
-          priceRule {
-            id
-          }
-          priceRuleDiscountCode {
-            id
-            code
-            usageCount
-          }
-          priceRuleUserErrors {
-            code
-            field
-            message
+          priceRuleCreate(
+            priceRule: $priceRule,
+            priceRuleDiscountCode: $priceRuleDiscountCode
+          ) {
+            priceRule {
+              id
+            }
+            priceRuleDiscountCode {
+              id
+              code
+              usageCount
+            }
+            priceRuleUserErrors {
+              code
+              field
+              message
+            }
           }
         }
-      }
-    `,
-    variables: {
-      priceRule: {
-        allocationMethod: 'EACH',
-        customerSelection: {
-          forAllCustomers: true,
+      `,
+      variables: {
+        priceRule: {
+          allocationMethod: 'EACH',
+          customerSelection: {
+            forAllCustomers: true,
+          },
+          itemEntitlements: {
+            productIds: ['gid://shopify/Product/4871257882763'],
+          },
+          target: 'LINE_ITEM',
+          title: `Free Stickers - Achievement #${achievementId}`,
+          usageLimit: 1,
+          validityPeriod: {
+            start: new Date(),
+          },
+          value: {
+            percentageValue: -100,
+          },
         },
-        itemEntitlements: {
-          productIds: ['gid://shopify/Product/4871257882763'],
-        },
-        target: 'LINE_ITEM',
-        title: `Free Stickers - Achievement #${achievementId}`,
-        usageLimit: 1,
-        validityPeriod: {
-          start: new Date(),
-        },
-        value: {
-          percentageValue: -100,
+        priceRuleDiscountCode: {
+          code: uuidv4(),
         },
       },
-      priceRuleDiscountCode: {
-        code: uuidv4(),
-      },
-    },
-  });
+    });
 
-  return newDiscountCode;
+    return newDiscountCode;
+  } catch (error) {
+    console.error(`Shopify Discount Code Creation: `, error);
+  }
 };
